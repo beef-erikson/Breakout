89b793dfb83006501e7311a4dccac5ece4825f4f
Troy Martin <beef.erikson.studios@gmail.com>
2019-06-24 15:52:00 -0400

    Sprites and Paddle logic start
    Not currently working

---
+++ Assets/Scripts/Gameplay/Paddle.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// Movement of Paddle
+/// </summary>
+public class Paddle : MonoBehaviour
+{
+    #region Fields
+
+    Rigidbody2D rb2d;
+
+    #endregion
+
+    #region Methods
+
+    /// <summary>
+    /// Grabs rigidbody
+    /// </summary>
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            rb2d.MovePosition(new Vector2(transform.position.x + ConfigurationUtils.PaddleMoveUnitsPerSecond * Time.deltaTime,
+                transform.position.y));
+        }
+    }
+
+    #endregion
+}
