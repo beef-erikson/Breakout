5f01c55bbec095a105b084d59801f3b059f2e73a
Troy Martin <beef.erikson.studios@gmail.com>
2019-06-25 13:19:15 -0400

    Blocks now break

---
+++ Assets/Scripts/Gameplay/Block.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Block destruction
+/// </summary>
+public class Block : MonoBehaviour
+{
+    /// <summary>
+    /// Destroys block
+    /// </summary>
+    /// <param name="collision">collision</param>
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);    
+    }
+}
